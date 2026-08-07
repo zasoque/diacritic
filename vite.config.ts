b7d6f1bb999d8f99577b940928151a95cea7b4da
Sch <junhg0211@gmail.com>
2026-08-07 16:45:22 +0900

    Rename adapter from auto to static in vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
