811a2b124e1c0fb452969a79300851ae08f17eae
Sch <junhg0211@gmail.com>
2026-08-07 16:32:46 +0900

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<style>
+		body {
+			margin: 0;
+		}
+	</style>
+</svelte:head>
+
+{@render children()}
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import adapter from '@sveltejs/adapter-auto';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [
+		sveltekit({
+			compilerOptions: {
+				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+				runes: ({ filename }) =>
+					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+			},
+			adapter: adapter()
+		})
+	],
+	server: {
+		allowedHosts: ['sch.shtelo.org']
+	}
+});
