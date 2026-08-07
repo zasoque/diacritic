16f78a1f84ab588fb61002faf5a5e4513718f24b
Sch <junhg0211@gmail.com>
2026-08-07 16:43:20 +0900

    config

--- svelte.config.ts
+++ svelte.config.ts
@@ -2,7 +2,15 @@ import adapter from '@sveltejs/adapter-static';
 
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
 	}
 };
 
